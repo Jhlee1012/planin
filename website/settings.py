6d09de7e214f3649d94b927714c4fc62f8801917
Jhlee1012 <leejinhyun327@gmail.com>
2022-05-04 10:09:07 +0900

    캘린더 업로드

--- website/settings.py
+++ website/settings.py
@@ -77,9 +77,17 @@ WSGI_APPLICATION = 'website.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'HOST' : 'aws-test.cz1meqdda4p7.us-west-2.rds.amazonaws.com',
+        'PORT' : '3306',
+        'NAME' : 'demo_rds',
+        'USER' : 'demouser',
+        'PASSWORD' : "1234",
     }
 }
 
